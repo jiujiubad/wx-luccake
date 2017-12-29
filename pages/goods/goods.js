06b6de8b81045e47925b549d9fe27847bed45c39
谢育初 <524945638@qq.com>
2017-12-29 10:51:19 +0800

    api rewrite

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -2,33 +2,11 @@
 Page({
 
   data: {
-    isLike: true,
-    // banner
-    imgUrls: [
-      "http://mz.djmall.xmisp.cn/files/product/20161201/148057921620_middle.jpg",
-      "http://mz.djmall.xmisp.cn/files/product/20161201/148057922659_middle.jpg",
-      "http://mz.djmall.xmisp.cn/files/product/20161201/148057923813_middle.jpg",
-      "http://mz.djmall.xmisp.cn/files/product/20161201/148057924965_middle.jpg",
-      "http://mz.djmall.xmisp.cn/files/product/20161201/148057925958_middle.jpg"
-    ],
-    indicatorDots: true, //是否显示面板指示点
-    autoplay: true, //是否自动切换
-    interval: 3000, //自动切换时间间隔,3s
-    duration: 1000, //  滑动动画时长1s
 
-    // 商品详情介绍
-    detailImg: [
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_1.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_2.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_3.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_4.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_5.jpg",
-      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_6.jpg",
-    ],    
   },
 
   onLoad: function (options) {
-    console.log(options.id)
+    console.log(options)
     var p_id = options.id
     var that = this
     wx.request({
@@ -37,43 +15,13 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data.photos)
+        console.log(res.data)
         that.setData({
           picdata: res.data.photos,
           goods: res.data
         })
       }
     })
-  },
-
-  //预览图片
-  previewImage: function (e) {
-    var current = e.target.dataset.src;
-
-    wx.previewImage({
-      current: current, // 当前显示图片的http链接  
-      urls: this.data.imgUrls // 需要预览的图片http链接列表  
-    })
-  },
-  // 收藏
-  addLike() {
-    this.setData({
-      isLike: !this.data.isLike
-    });
-  },
-  // 跳到购物车
-  toCar() {
-    wx.switchTab({
-      url: '/pages/cart/cart'
-    })
-  },
-  // 立即购买
-  immeBuy() {
-    wx.showToast({
-      title: '购买成功',
-      icon: 'success',
-      duration: 2000
-    });
-  },  
+  }
 
 })
