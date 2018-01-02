6e557df0d8de670f4fad38f5298da3b7f76b16b8
谢育初 <524945638@qq.com>
2018-01-02 14:10:45 +0800

    p_search page head

--- pages/p_search/p_search.js
+++ pages/p_search/p_search.js
@@ -1,66 +1,32 @@
-// pages/p_search/p_search.js
+var API_URL = 'https://luccake.top/api/v1/products/search'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    cakes: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  search: function (e) {
+    var aaa = API_URL + "?utf8=✓&q=" + e.detail.value
+    console.log(aaa)
+    if (!e.detail.value) {
+      return;
+    }
+    wx.showToast({ //加载中的动画效果
+      title: "加载中..",
+      icon: "loading",
+      duration: 10000
+    });
+    var that = this; //保存this的数据
+    wx.request({
+      url: API_URL + "?utf8=✓&q=" + e.detail.value,
+      header: {
+        'content-type': 'json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        wx.hideToast();
+        that.setData({
+          cakes: res.data.data
+        })
+      }
+    })
   }
 })
