da43ef8c47cb2602cbcbd5a69bd335d949cfb250
谢育初 <524945638@qq.com>
2018-01-05 00:23:32 +0800

    page/p_result can sort by all/price/created_at/quantiy

--- pages/p_result/p_result.js
+++ pages/p_result/p_result.js
@@ -1,14 +1,19 @@
 var API_URL = 'https://luccake.top/api/v1/products?utf8=%E2%9C%93&q%5Btitle_or_description_cont%5D='
+var API_URL2 = 'https://luccake.top/api/v1/products?commit=Search&q%5Bs%5D='
+var API_com1 = '&q%5Btitle_or_description_cont%5D='
+var API_com2 = '&utf8=%E2%9C%93'
 
 Page({
   data:{
     cakes: [],
     result: [],
     name: '',
+    currentTab: 0,
   },
   onLoad:function(e){
-    var inputValue = e.arr
-    console.log("成功传递",e.arr)
+    var inputValue = e.str
+    wx.setStorageSync('inputValue', inputValue);
+    console.log("成功传递",e.str)
     if (!inputValue) {
       return;
     }
@@ -27,14 +32,17 @@ Page({
       success: function (res) {
         wx.hideToast();
         that.setData({
-          cakes: res.data.data
+          cakes: res.data.data,
+          name: e.str
         })
       }
     })
   },
-  search2: function (e) {
-    console.log("结果",e)
-    if (!e.detail.value) {
+  search: function (e) {
+    var inputValue = e.detail.value
+    console.log("重新搜索", inputValue)
+    wx.setStorageSync('inputValue', inputValue);
+    if (!inputValue) {
       return;
     }
     wx.showToast({ //加载中的动画效果
@@ -43,7 +51,7 @@ Page({
       duration: 10000
     });
     var that = this; //保存this的数据
-    var utf = encodeURI(e.detail.value)
+    var utf = encodeURI(inputValue)
     wx.request({
       url: API_URL + utf + "&commit=Search",
       header: {
@@ -52,13 +60,104 @@ Page({
       success: function (res) {
         wx.hideToast();
         that.setData({
-          cakes: res.data.data
+          cakes: res.data.data,
+          currentTab: 0,
+          name: inputValue
         })
       }
     })
   },
-  resetSearch2: function () {
+  resetSearch: function () {
     var result = new Array();
     this.setData({ result: result, name: '' });
+  },
+
+  switchAll: function (e) {
+    var inputValue = this.data.name
+    console.log(inputValue)
+    if (!inputValue) {
+      return;
+    }
+    var that = this; //保存this的数据
+    var utf = encodeURI(inputValue)
+    wx.request({
+      url: API_URL + utf + "&commit=Search",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          cakes: res.data.data,
+          currentTab: 0
+        })
+      }
+    })
+  },
+  switchPrice:function(e){
+    var inputValue = this.data.name
+    console.log(inputValue)
+    if (!inputValue) {
+      return;
+    }
+    var that = this; //保存this的数据
+    var utf = encodeURI(inputValue)
+    wx.request({
+      url: API_URL2+'price+asc'+API_com1+utf+API_com2,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          cakes: res.data.data,
+          currentTab: 1
+        })
+      }
+    })
+  },
+  switchTime: function (e) {
+    var inputValue = this.data.name
+    console.log("按时间排序", inputValue)
+    if (!inputValue) {
+      return;
+    }
+    var that = this; //保存this的数据
+    var utf = encodeURI(inputValue)
+    wx.request({
+      url: API_URL2 + 'created_at+desc' + API_com1 + utf + API_com2,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          cakes: res.data.data,
+          currentTab: 2
+        })
+      }
+    })
+  },
+  switchTest:function(e){
+    var inputValue = this.data.name
+    console.log("按时间排序", inputValue)
+    if (!inputValue) {
+      return;
+    }
+    var that = this; //保存this的数据
+    var utf = encodeURI(inputValue)
+    wx.request({
+      url: API_URL2 + 'quantity+desc' + API_com1 + utf + API_com2,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          cakes: res.data.data,
+          currentTab: 3
+        })
+      }
+    })
   }
 })
--- pages/p_search/p_search.js
+++ pages/p_search/p_search.js
@@ -4,27 +4,16 @@ Page({
   data: {
     cakes: [],
     result: [],
-    name: ''
+    name: '',
   },
   onLoad:function(){
     this.loadTips();
-  },
-  search: function (e) {
-    var inputValue = e.detail.value
-    wx.navigateTo({
-      url: '../p_result/p_result?arr=' + inputValue,
-    })
-    console.log("ff", this.data)
-  },
-  resetSearch: function () {
-    var result = new Array();
-    this.setData({ result: result, name: '' });
   },
   loadTips:function(e){
     var that = this;
     var word = [];
     wx.request({
-      url: 'https://luccake.top/api/v1/products', 
+      url: 'https://luccake.top/api/v1/products',
       header: {
         'content-type': 'application/json' // 默认值
       },
@@ -37,12 +26,96 @@ Page({
     })
   },
   clickTip:function(e){
-    console.log(e.currentTarget.dataset.title)
     var name = e.currentTarget.dataset.title
-    wx.navigateTo({
-      url: '../p_result/p_result?arr=' + name,
+    if (!name) {
+      return;
+    }
+    wx.showToast({ //加载中的动画效果
+      title: "加载中..",
+      icon: "loading",
+      duration: 10000
+    });
+    var that = this; //保存this的数据
+    var utf = encodeURI(name)
+    wx.request({
+      url: API_URL + utf + "&commit=Search",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        wx.hideToast();
+        that.setData({          
+          cakes: res.data.data,
+          name: name,
+        })
+      }
     })
+  },
+  loadTitles:function(e){
+    var that = this; //保存this的数据
+    wx.request({
+      url: 'https://luccake.top/api/v1/products',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        that.setData({
+          goods: res.data.data,
+        }) 
+        wx.setStorageSync('e',res.data.data)
+      }
+    })
+    var goods = wx.getStorageSync('e');
+    var titles = [];
+    for (var i = 0; i < goods.length; i++) {
+      var title = goods[i].title;
+      titles.push(title);
+    }
+    this.setData({ titles: titles })
+    console.log('test', titles)
+    return titles 
+  },
+  searchTitles: function (e) {
+    var name = e.detail.value;
+    var titles = this.loadTitles();
+    var result = [];
+    if (name != '') {
+      for (var i = 0; i < titles.length; i++) {
+        var good = titles[i];
+        if (good.indexOf(name) > -1) {
+          result.push(good);
+        }
+      }
+      this.setData({ result: result });
+    }
+    if (result == ''){
+      if (!name) {
+        return;
+      }
+      wx.showToast({ //加载中的动画效果
+        title: "加载中..",
+        icon: "loading",
+        duration: 10000
+      });
+      var that = this; //保存this的数据
+      var utf = encodeURI(name)
+      wx.request({
+        url: API_URL + utf + "&commit=Search",
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          wx.hideToast();
+          that.setData({
+            cakes: res.data.data,
+            name: name
+          })
+        }
+      })
+    }
+  },
+  resetSearch: function () {
+    var result = [];
+    this.setData({ result: result, name: '' });
   }
 })
-
-
