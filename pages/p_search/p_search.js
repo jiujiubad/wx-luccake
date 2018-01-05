4da603e5a277c6f4f3df4024eb1f09946acc2167
谢育初 <524945638@qq.com>
2018-01-05 21:51:11 +0800

    p_search add remarks( can easy read code)

--- pages/p_search/p_search.js
+++ pages/p_search/p_search.js
@@ -11,13 +11,12 @@ Page({
     currentTab: 0,
   },
   onLoad:function(){
-    this.loadTips();
-    var searchData = wx.getStorageSync('searchData');
-    searchData.reverse()
+    this.loadTips();  //加载“热门搜索”函数
+    var searchData = wx.getStorageSync('searchData'); //获取缓存里的”历史搜索”数组，分别在clickTip、clickTitle、clickSearchData、searchTitle的函数里设置wx.setStorageSync
+    searchData.reverse()  //数组顺序颠倒（反转）
     this.setData({
       searchData: searchData,
     })
-    console.log('sea',searchData)
   },
   loadTips:function(e){
     var that = this;
@@ -25,20 +24,19 @@ Page({
     wx.request({
       url: 'https://luccake.top/api/v1/products',
       header: {
-        'content-type': 'application/json' // 默认值
+        'content-type': 'application/json'
       },
       success: function (res) {
-        wx.setStorageSync('word',res.data.data)
+        wx.setStorageSync('word',res.data.data)  //success function等回调函数是不能return返回数据的，可以通过缓存的方法拿到数据。
       }
     })
     var word = wx.getStorageSync('word');
     var out = [];
     for(var i=0; i<10; i++){
-      var temp = (Math.random()*word.length)>>0;
-      out.push(word[temp]);
+      var temp = (Math.random()*word.length)>>0; //随机产生0-word.length的数字。其中Math.random()是js方法，随机产生0-1的数。
+      out.push(word[temp]); //把word[temp]放在out数组的最右边，组成新的out数组
     }
     this.setData({out: out})
-    console.log('cccfd',out)
   },
   clickTip:function(e){
     var name = e.currentTarget.dataset.title
@@ -48,8 +46,7 @@ Page({
     var searchData = wx.getStorageSync('searchData') || []
     searchData.push(name)
     wx.setStorageSync('searchData', searchData)
-    console.log('ffff',searchData)
-    wx.showToast({ //加载中的动画效果
+    wx.showToast({ //显示”加载中“”的动画效果
       title: "加载中..",
       icon: "loading",
       duration: 10000
@@ -59,28 +56,26 @@ Page({
     wx.request({
       url: API_URL + utf + "&commit=Search",
       header: {
-        'content-type': 'application/json' // 默认值
+        'content-type': 'application/json'
       },
       success: function (res) {
-        wx.hideToast();
+        wx.hideToast(); //隐藏”加载中“”的动画效果
         that.setData({
           cakes: res.data.data,
           name: name,
-          word: ''
+          word: '' //小技巧，word清空，意味着wxml不会显示wx:for的数据。
         })
       }
     })
   },
   clickSearchData:function(e){
-    console.log(e)
-    var name = e.currentTarget.dataset.title
+    var name = e.currentTarget.dataset.title //通过console.log获得e的散列，展开得到tilte的写法
     if (!name) {
       return;
     }
     var searchData = wx.getStorageSync('searchData') || []
     searchData.push(name)
     wx.setStorageSync('searchData', searchData)
-    console.log('ffff',searchData)
     wx.showToast({ //加载中的动画效果
       title: "加载中..",
       icon: "loading",
@@ -98,7 +93,7 @@ Page({
         that.setData({
           cakes: res.data.data,
           name: name,
-          word: ''
+          word: '' //小技巧，清空，意味着wxml不会显示wx:for的数据。
         })
       }
     })
@@ -114,12 +109,12 @@ Page({
         that.setData({
           goods: res.data.data,
         })
-        wx.setStorageSync('e',res.data.data)
+        wx.setStorageSync('e',res.data.data)  //在api回调函数里设置缓存数组，目的是拿到数组里的title。
       }
     })
     var goods = wx.getStorageSync('e');
     var titles = [];
-    for (var i = 0; i < goods.length; i++) {
+    for (var i = 0; i < goods.length; i++) { //循环拿到title数组
       var title = goods[i].title;
       titles.push(title);
     }
@@ -133,15 +128,14 @@ Page({
     if (name != '') {
       for (var i = 0; i < titles.length; i++) {
         var good = titles[i];
-        if (good.indexOf(name) > -1) {
+        if (good.indexOf(name) > -1) {  //js的indexOf()方法，看输入值name是否在标题库title中，-1表示检索的字符串值没有出现，0表示匹配且从第0个字符就匹配。
           result.push(good);
         }
       }
       this.setData({ result: result });
-      var searchData = wx.getStorageSync('searchData') || []
+      var searchData = wx.getStorageSync('searchData') || [] //以下三行缓存，同上几个click函数
       searchData.push(name)
       wx.setStorageSync('searchData', searchData)
-      console.log('dad',searchData)
     }
     if (result == ''){
       if (!name) {
@@ -157,7 +151,7 @@ Page({
       wx.request({
         url: API_URL + utf + "&commit=Search",
         header: {
-          'content-type': 'application/json' // 默认值
+          'content-type': 'application/json'
         },
         success: function (res) {
           wx.hideToast();
@@ -170,13 +164,11 @@ Page({
     }
   },
   clickTitle:function(e){
-    console.log(e)
     var name = e.currentTarget.dataset.title
-    console.log(e.currentTarget.dataset.title)
     if (!name) {
       return;
     }
-    var searchData = wx.getStorageSync('searchData') || []
+    var searchData = wx.getStorageSync('searchData') || []  //缓存输入值，同上几个click函数
     searchData.push(name)
     wx.setStorageSync('searchData', searchData)
     wx.showToast({ //加载中的动画效果
@@ -196,14 +188,14 @@ Page({
         that.setData({
           cakes: res.data.data,
           name: name,
-          result: ''
+          result: '' //小技巧，清空，意味着wxml不会显示wx:for的数据。
         })
       }
     })
   },
   resetSearch: function () {
     var result = [];
-    this.setData({ result: result, name: '' });
+    this.setData({ result: result, name: '' }); //清空匹配输入值，清空输入值
   },
 
 
@@ -211,9 +203,8 @@ Page({
 
 
 
-  switchAll: function (e) {
+  switchAll: function (e) { //排序-所有
     var inputValue = this.data.name
-    console.log(inputValue)
     if (!inputValue) {
       return;
     }
@@ -225,7 +216,6 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data)
         that.setData({
           cakes: res.data.data,
           currentTab: 0
@@ -233,9 +223,8 @@ Page({
       }
     })
   },
-  switchPrice:function(e){
+  switchPrice:function(e){ //排序-价格
     var inputValue = this.data.name
-    console.log(inputValue)
     if (!inputValue) {
       return;
     }
@@ -247,7 +236,6 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data)
         that.setData({
           cakes: res.data.data,
           currentTab: 1
@@ -255,9 +243,8 @@ Page({
       }
     })
   },
-  switchTime: function (e) {
+  switchTime: function (e) { //排序-时间
     var inputValue = this.data.name
-    console.log("按时间排序", inputValue)
     if (!inputValue) {
       return;
     }
@@ -269,7 +256,6 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data)
         that.setData({
           cakes: res.data.data,
           currentTab: 2
@@ -277,9 +263,8 @@ Page({
       }
     })
   },
-  switchTest:function(e){
+  switchTest:function(e){ //排序-测试
     var inputValue = this.data.name
-    console.log("按时间排序", inputValue)
     if (!inputValue) {
       return;
     }
@@ -291,7 +276,6 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data)
         that.setData({
           cakes: res.data.data,
           currentTab: 3
