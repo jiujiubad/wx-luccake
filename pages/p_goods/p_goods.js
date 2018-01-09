57c47dc0ed2cf746addc421d24ac39e16db08b18
谢育初 <524945638@qq.com>
2018-01-09 14:16:51 +0800

    pages/goods

--- pages/p_goods/p_goods.js
+++ pages/p_goods/p_goods.js
@@ -2,14 +2,14 @@
 Page({
 
   data: {
-
+    goods:[]
   },
   onPullDownRefresh: function () {
     wx.showNavigationBarLoading();// 显示导航栏loading
     this.onLoad();// 调用接口加载数据
     wx.hideNavigationBarLoading();// 隐藏导航栏loading
     wx.stopPullDownRefresh();// 当处理完数据刷新后，停止当前页面的下拉刷新
-  },  
+  },
   onLoad: function (options) {
     console.log(options)
     var p_id = options.id
