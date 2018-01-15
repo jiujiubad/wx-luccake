1fe659e04851fc922eb94a2dbf61c13b99e25a9b
谢育初 <524945638@qq.com>
2018-01-15 19:41:54 +0800

    page/p_goods random product function

--- pages/p_goods/p_goods.js
+++ pages/p_goods/p_goods.js
@@ -20,13 +20,33 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data)
+        console.log('fff',res.data)
         that.setData({
           picdata: res.data.photos,
           goods: res.data
         })
       }
     })
-  }
-
+    that.random_8()
+  },
+  random_8:function(e){ //加载“热门搜索”
+    var that = this;
+    wx.request({
+      url: 'https://luccake.top/api/v1/products',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        wx.setStorageSync('word',res.data.data)  //success function等回调函数是不能return返回数据的，可以通过缓存的方法拿到数据。
+      }
+    })
+    var word = wx.getStorageSync('word');
+    var ran = [];
+    for(var i=0; i<10; i++){
+      var temp = (Math.random()*word.length)>>0; //随机产生0-word.length的数字。其中Math.random()是js方法，随机产生0-1的数。
+      ran.push(word[temp]); //把word[temp]放在out数组的最右边，组成新的out数组
+      console.log('ran',ran)
+    }
+    this.setData({ran:ran})
+  },
 })
